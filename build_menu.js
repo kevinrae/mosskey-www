4825bbedb2176f9fecee987f18483858b6bfeb66
Kevin Rae <kevin.rae@powerreviews.com>
2017-02-26 23:02:09 -0800

    code cleanup & comments added to functions.

--- build_menu.js
+++ build_menu.js
@@ -4,13 +4,16 @@ function init() {
 }
 
 function addCharacterToSelectedPanel() {
+  var txtalrt = '';
   console.log(this);
   var id = $(this).data('id');
   var name = $(this).text();
   if ($('#selected_characters li').find("[data-selected-id='"+id+"']" ).length) {  
-    alert('you already selected :' + name);
+    txtalrt += 'You already selected ' + name;
+    txtalrt += ' and you only need to pick it once.  Please pick something else.'; 
+    alert(txtalrt);
   } else {
-   $('#selected_characters').prepend('<li data-selected-id="'+id+'">'+name+'<a class="remove_character" data-selected-id="'+id+'" href="#"> [X]</a></li>');
+    $('#selected_characters').prepend('<li data-selected-id="'+id+'">'+name+'<a class="remove_character" data-selected-id="'+id+'" href="#"> [X]</a></li>');
   }
 }
 
@@ -19,13 +22,28 @@ function removeCharacter() {
 }
 
 function parsetree(data) {
+/* Parsetree is invoked via callback from the php script used for making the
+   sql query to the db.  It builds the dynamic dropdown menus (& submenus) 
+   using bootstrap components.  Contains a nested function for closing <li> 
+   & <ul> tags.  Also... this parses jsonp data where each row has this
+   format:
+     id, name, lft, rht, depth
+   lft, rht values define a tree structure.  reverse preorder tree traversal 
+   algorithm is used to navigate thru the tree and build the dropdowns.
+   
+   If/when debugging, it is useful to output lft, rht, depth values and/or 
+   review the console.log  */
+
   var row, txt = "";
-  var depth;
-//  var parentrht, grandparentrht;
+//  var depth;
   var stack = [];
   txt += '<ul class="dropdown-menu">';
 
   /// nested function//////////////////////////////////////////
+  // the last (rightmost) child of a parent will have:
+  // child.rht + 1 = parent.rht
+  // these denote the last list entry for the ul, and therefore
+  // we need to close tags 
   function close_tags_for_last_child_in_tree(stack, rht) {
     var parentrht = stack.pop();
     if (parentrht - rht == 1) {           // last child in tree 
@@ -34,8 +52,7 @@ function parsetree(data) {
     } else { 
       stack.push(parentrht);
     }
-  }
-  //////////////////////////////////////////////////////////
+  } //////////////////////////////////////////////////////////
 
   for (row in data) {
     console.log(data[row]);
@@ -43,57 +60,26 @@ function parsetree(data) {
       txt += '<li class="dropdown-submenu">';
       txt += '<a class="character" data-id="' + data[row].id; 
       txt += ' name="' + data[row].name + '">';
-      //////////////////////////////////////////////////////////
-      // indent logic for tree display - short-term work around 
-      // until we can get dynamic dropdown submenus working
-      for (i = 1; i < data[row].depth; i++){
-        txt += " . ";
-      } ///////////////////////////////////////////////////////
       txt += data[row].name;
 //      txt += ' data-id: '+ data[row].id;
-      txt += ' lft: '+ data[row].lft;
-      txt += ' rht: '+ data[row].rht;
+//      txt += ' lft: '+ data[row].lft;
+//      txt += ' rht: '+ data[row].rht;
 //      txt += ' depth: '+ data[row].depth;
       txt += '</a>'; 
       txt += '<ul class="dropdown-menu">';
-      // store rht value in stack for parents
-      stack.push(data[row].rht);
-      } else {
-      // leaf test
+      stack.push(data[row].rht); // store rht in stack for parents
+      } else { // this is a leaf node 
       txt += '<li><a class="character" data-id="' + data[row].id; 
       txt += '" href="#"';
       txt += ' name="' + data[row].name + '">';
-      //////////////////////////////////////////////////////////
-      // indent logic for tree display - short-term work around 
-      // until we can get dynamic dropdown submenus working
-      for (i = 1; i < data[row].depth; i++){
-        txt += " . ";
-      } ///////////////////////////////////////////////////////
       txt += data[row].name;
 //      txt += ' data-id: '+ data[row].id;
-      txt += ' lft: '+ data[row].lft;
-      txt += ' rht: '+ data[row].rht;
+//      txt += ' lft: '+ data[row].lft;
+//      txt += ' rht: '+ data[row].rht;
 //      txt += ' depth: '+ data[row].depth;
       txt += '</a></li>'; 
 
       close_tags_for_last_child_in_tree(stack, data[row].rht);
-/*
-      parentrht = stack.pop();
-      if (parentrht - data[row].rht == 1) { // last child in tree 
-        txt += '</ul></li>';                // close tags
-      //////////////////////////////////////////////////////////
-      // check to see if we need to close a grandparent too
-      // note - only works for 3-level trees...
-        grandparentrht = stack.pop();
-        if (grandparentrht - parentrht == 1){
-          txt += '</ul></li>';                // close tags
-        } else {
-          stack.push(grandparentrht); // replace parent.rht in stack 
-        } //////////////////////////////////////////////////////
-      } else {
-        stack.push(parentrht); // replace parent.rht in stack 
-      }
-*/
     }
   }  
   txt += '</ul>';
@@ -104,38 +90,3 @@ function parsetree(data) {
 //http://stackoverflow.com/questions/11128700/create-a-ul-and-fill-it-based-on-a-passed-array
 
 //cannot add onclick attribute to href here and expect this variable to get properly scoped
-function format_unordered_list_menu(data) {
-  var row, txt = "";
-  var depth;
-  txt += '<ul class=dropdown-menu>';
-  for (row in data) {
-    // leaf test
-    // parent test
-    // store rht value in stack for parents
-    // if (parent.rht - data[row].rht == 1) // close the </ul>
-
-
-//    if (data[row].rht - data[row].lft == 1) {// leaf node?
-      txt += '<li><a class="character" data-id="' + data[row].id; 
-      txt += '" href="#"';
-      txt += ' name="' + data[row].name + '">';
-      //////////////////////////////////////////////////////////
-      // indent logic for tree display - short-term work around until we can get dynamic dropdown submenus working
-      for (i = 1; i < data[row].depth; i++){
-        txt += " . ";
-      }
-      //////////////////////////////////////////////////////////
-      txt += data[row].name;
-      txt += ' data-id: '+ data[row].id;
-      txt += ' lft: '+ data[row].lft;
-      txt += ' rht: '+ data[row].rht;
-      txt += ' depth: '+ data[row].depth;
-      txt += '</a></li>'; 
-//    } else { // parent?
-//      txt += '<li class="dropdown-submenu"><a class="character" id="' + data[row].id + '" href="#" onclick="addCharacterToPanel()" name="' + data[row].name + '">'+ data[row].name + '</a>';
-//      txt += '<ul class="dropdown-menu">'; 
-//    }
-  }
-  txt += "</ul>";
-  $('#dynamic_menu_button').after(txt);
-}
