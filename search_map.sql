ce18355c605fe7aaf412e30427d60f8c5d50ae05
Kevin Rae <kevin.rae@powerreviews.com>
2018-01-22 15:25:01 -0800

    more file cleanup

--- search_map.sql
+++
@@ -1,4 +0,0 @@
-use mosskey;
-SELECT Taxa.Name, Taxa.id, Map.KeyCharacterId FROM Taxa, Map WHERE 
-Map.TaxaId = Taxa.id AND
-Map.KeyCharacterId IN ('3','4','5');
--- show-all-keycharacters-for-single-taxa-id.sql
+++
@@ -1,6 +0,0 @@
-use mosskey;
-select keycharacter.name FROM map, keycharacter 
-WHERE 
-map.keycharacterID=keycharacter.id
-AND
-map.TaxaId=2; 
