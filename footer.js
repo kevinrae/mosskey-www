b7e42a88c1e9bf2f30b1309bb236bed0efdd0d26
Kevin Rae <kevin.rae@powerreviews.com>
2018-03-03 18:22:55 -0800

    issue #18 - separated references page from about. updated navbar code.  tried to make it modularbut ran into troulble displaying active menu item.  may have something to do with bootstrap code.  or page loading of navigation html after execution of footer.js code with link selection jquery code.  anyway, could not make it work so punting on modularizing header code.

--- footer.js
+++ footer.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+//  $(".navigation").load("navigation.html"); 
   $(".footer").load("footer.html");
+
+// doesn't work :( /////////////////////////////
+  $(".nav a").on("click", function(){
+     $(".nav").find(".active").removeClass("active");
+     $(this).parent().addClass("active");
+  });
+////////////////////////////////////////////////
+
 });
